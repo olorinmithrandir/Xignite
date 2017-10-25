2cd13565be86e8e4b2f07ccc10bc56a47e4aee52
appdc2c <pratik_shah@troweprice.com>
2017-10-25 11:46:56 -0500

    first

---
+++ src/com/trp/mstar/builder/ASInvVehicleBuilder.java
@@ -0,0 +1,40 @@
+package com.serverless.mstar.builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.serverless.mstar.domain.ASInvVehicle;
+
+public class ASInvVehicleBuilder {
+	
+	
+	private String line;
+	
+	public ASInvVehicleBuilder(){
+		
+	}
+	
+	
+	private ASInvVehicleBuilder(String line){
+		this.line=line;
+	}
+	
+	public ASInvVehicleBuilder withLine(String line){
+		
+		return new ASInvVehicleBuilder(line);
+	}
+	
+	
+	public List<ASInvVehicle> build(){
+	  
+		List<ASInvVehicle> asInvVehicles=new ArrayList();
+	  
+		String[] data=line.split(",");
+		ASInvVehicle asInvVehicle=new ASInvVehicle();
+		asInvVehicle.setIvName(data[0]);
+		
+	  return asInvVehicles;
+	}
+	
+
+}
---
+++ src/com/trp/mstar/rest/service/XigniteService.java
@@ -0,0 +1,31 @@
+package com.serverless.mstar.rest.service;
+
+import java.util.List;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.serverless.mstar.domain.ExchangeResult;
+import com.serverless.mstar.domain.Exchanges;
+
+public class XigniteService {
+
+	
+	RestTemplate restTemplate=new RestTemplate();
+	
+	public ExchangeResult getExchanges(){
+		return this.restTemplate.getForObject("https://factsetestimates.xignite.com/xFactSetEstimates.json/ListExchanges?&_token=AE4A02E0271A4E77B78B314AEE9A132D",ExchangeResult.class);
+		
+		
+	}
+	
+	public String getExchangesAsStr(){
+		return this.restTemplate.getForObject("https://factsetestimates.xignite.com/xFactSetEstimates.json/ListExchanges?&_token=AE4A02E0271A4E77B78B314AEE9A132D",String.class);
+		
+		
+	}
+	
+	public static void main(String s[]){
+		System.out.println("exchanges r "+new XigniteService().getExchanges());
+	}
+	
+}
